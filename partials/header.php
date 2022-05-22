f0bf932af57034c9c519b44b1cf86351cdd875c7
Fahmilu Kurniawan <fahmi@catalyzecommunications.com>
2022-05-22 13:21:26 +0700

    main

--- partials/header.php
+++ partials/header.php
@@ -1,12 +1,17 @@
 <!-- this is header section -->
 <header>
-    <nav class="navbar navbar-expand-md p-0">
+    <nav class="navbar navbar-expand-lg p-0">
         <div class="container">
-            <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-                aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-md-between" id="collapsibleNavId">
+            <div class="d-flex d-lg-none justify-content-between w-100">
+                <a href="/"><img src="assets/images/logo.svg" alt="logo" srcset="assets/images/logo.png" class="logo d-block"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse justify-content-md-between" id="navbar">
+                <div class="d-block d-lg-none">
+                    change language
+                </div>
                 <ul class="navbar-nav mt-2 mt-lg-0">
                     <li class="nav-item active">
                         <a class="nav-link" href="?page=about">Tentang Kami</a>
@@ -24,8 +29,8 @@
                         <a class="nav-link" href="?page=shop">Belanja</a>
                     </li>
                 </ul>
-                <a href="/"><img src="assets/images/logo.svg" alt="logo" srcset="assets/images/logo.png" class="logo d-block"></a>
-                <div class="d-flex align-items-center">
+                <a href="/" class="d-none d-lg-block"><img src="assets/images/logo.svg" alt="logo" srcset="assets/images/logo.png" class="logo d-block"></a>
+                <div class="d-none d-lg-flex align-items-center">
                     <form class="d-flex search" action="/" method="get">
                         <input type="hidden" name="page" value="search" />
                         <input class="form-control me-sm-2" name="q" type="text" placeholder="Mau cari apa?">
@@ -36,10 +41,8 @@
                         <a class="nav-link active" href="#">ID</a>
                     </div>
                 </div>
-
             </div>
         </div>
     </nav>
 </header>
-
 <!-- end of header section -->
